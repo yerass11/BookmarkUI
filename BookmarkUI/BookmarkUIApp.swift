282b0eeb20c713dd5513cddd2af31989f2dc8ec6
Yerassyl <Erasylsaiman2005@gmail.com>
2024-02-04 19:51:06 +0600

    Initial Commit

---
+++ BookmarkUI/BookmarkUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkUIApp.swift
+//  BookmarkUI
+//
+//  Created by Yerasyl on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
